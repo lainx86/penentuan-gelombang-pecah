b66a2217ed12bd4559ba4b842e5381e07ce92fe5
lain <febysyarief.dev@gmail.com>
2026-03-01 06:21:25 +0700

    Delete build directory

--- build/CMakeFiles/embed_data.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/embed_data"
-  "generated/embedded_data.h"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/embed_data.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
